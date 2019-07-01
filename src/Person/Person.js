589b48fd62a40167057e63b17e1a9bb8eceaa706
jantico <jantico@intralinks.com>
2019-07-01 11:05:01 -0400

    Commiting this file too so I can use CSS Modules

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Radium from 'radium';
 
 const person = (props) => {
     const style = {
@@ -17,4 +16,4 @@ const person = (props) => {
     )
 };
 
-export default Radium(person);
+export default person;
